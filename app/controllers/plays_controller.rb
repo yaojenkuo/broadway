c419b1c31c62985ae32b85ddb9036e20e339f0ed
yaojenkuo <tonykuoyj@gmail.com>
2017-06-02 16:57:07 +0800

    added edit and delete actions

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -22,6 +22,23 @@ class PlaysController < ApplicationController
         end
     end
     
+    def edit
+    end
+    
+    def update
+        if @play.update(play_params)
+            redirect_to play_path(@play)
+        else
+            render 'edit'
+        end
+        
+    end
+    
+    def destroy
+        @play.destroy
+        redirect_to root_path
+    end
+
     private
     def play_params
         params.require(:play).permit(:title, :description, :director)
