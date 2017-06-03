878b1dfc768d9a58e3df394ead8f2dded06797b6
yaojenkuo <tonykuoyj@gmail.com>
2017-06-03 10:45:37 +0800

    added paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602105328) do
+ActiveRecord::Schema.define(version: 20170603024223) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,10 +22,14 @@ ActiveRecord::Schema.define(version: 20170602105328) do
     t.string   "title"
     t.text     "description"
     t.string   "director"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.integer  "user_id"
     t.integer  "category_id"
+    t.string   "play_img_file_name"
+    t.string   "play_img_content_type"
+    t.integer  "play_img_file_size"
+    t.datetime "play_img_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
